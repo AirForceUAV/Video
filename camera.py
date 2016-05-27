a6ae711e19eb3a13f4d281e4141fd276a1679650
Pterosaur <ganze718@gmail.com>
2016-05-27 14:38:39 +0800

    test cammera

--- camera.py
+++ camera.py
@@ -9,17 +9,22 @@ def CaptureImages() :
     #load mqtt service
     videoClient = mqtt.Client()
     videoClient.reinitialise(client_id = "Camera", clean_session = True, userdata = None)
-    videoClient.connect("139.217.26.207", port = 1883, keepalive = 60, bind_address = "")
+    #videoClient.connect("139.217.26.207", port = 1883, keepalive = 60, bind_address = "")
+    videoClient.connect("192.168.31.243", port = 1883, keepalive = 60, bind_address = "")
     videoClient.loop_start()
 
-    #prepare the send buffer
-    output = StringIO.StringIO()
     #capture the images
-    camera = Camera()
+    camera = Camera(0)
+    i = 0
     while True:
+    	#prepare the send buffer
+	output = StringIO.StringIO()
         image = camera.getImage()
+	#print image.save("imgs/simple"+str(i)+".jpg")
+	#i += 1
         image.save(output)
-        videoClient.publish("Video", base64.b64decode(output.getvalue()))
+        videoClient.publish("Video", base64.b64encode(output.getvalue()))
+	output.close()
         time.sleep(.05)
 
 class Recover(object):
@@ -30,10 +35,10 @@ class Recover(object):
 
 videoProcess = os.fork()
 if videoProcess == 0 :
-    CaptureImages()
-    exit()
+	CaptureImages()
+	exit()
 
 videoRecover = Recover(videoProcess)
 if __name__ == "__main__" :
-    time.sleep(60)
+    time.sleep(1060)
 
